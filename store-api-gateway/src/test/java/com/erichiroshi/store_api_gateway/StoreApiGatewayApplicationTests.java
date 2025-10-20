bdcefbff772c33bb6cbdf76310784039fa5434e9
Eric Hiroshi <erichiroshi@hotmail.com>
2025-10-20 16:17:58 -0400

    api-gateway

---
+++ store-api-gateway/src/test/java/com/erichiroshi/store_api_gateway/StoreApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erichiroshi.store_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
